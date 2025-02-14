5aeb0f9b9048063a868cea0c8a646a69f52530f6
Crispy <christopherkhim668@gmail.com>
2025-02-14 20:21:27 +1100

    RESTARTING LMAOOOO

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".record").addEventListener("click", function() {
-    let countries = ["Japan", "Brazil", "France", "USA", "India"];
-    let randomCountry = countries[Math.floor(Math.random() * countries.length)];
-    document.querySelector(".label").innerText = randomCountry;
-});
